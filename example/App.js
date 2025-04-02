bfb26e978eac20521cf0b9769c0fa1b47bd1136a
carlos <c.delriofrances@gmail.com>
2025-04-02 13:25:57 -0300

    refactored

--- example/App.js
+++ example/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
-import { TabMotion } from '../src';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { TabMotion } from '../src/index';
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <TabMotion />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+});
 
 export default App;
